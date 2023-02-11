179f62e264501b3b80092e1cfa6eaa3178e36ab1
sagardas <sagardas.195@gmail.com>
2023-02-12 02:54:11 +0530

    formating

--- src/leedcodeEasy/LastDestination.java
+++ src/leedcodeEasy/LastDestination.java
@@ -1,2 +1,17 @@
-package leedcodeEasy;public class LastDestination {
+package leedcodeEasy;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.*;
+
+public class LastDestination {
+    public char repeatedCharacter(String s) {
+        Set<Character> set=new HashSet<>();
+        for(char c:s.toCharArray()){
+            if(set.add(c)){}else{
+                return c;
+            }
+        }
+        return 0;
+    }
 }
