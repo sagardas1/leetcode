dc9139253cb731bf810660805402ab59107d241b
sagardas <sagardas.195@gmail.com>
2023-02-12 14:18:54 +0530

    refactored

--- src/leedcodeEasy/NumberReversal.java
+++ src/leedcodeEasy/NumberReversal.java
@@ -3,21 +3,23 @@ package leedcodeEasy;
 class NumberReversal {
 
     public boolean isSameAfterReversals(int num) {
-        int i = reverse(num);
-        int j = reverse(i);
-        if (num == j) {
-            return true;
-        } else return false;
+        int i=reverse(num);
+        int j=reverse(i);
+if(num==j){
+    return true;
+}else return false;
     }
 
-    public static int reverse(int number) {
-        int reverse = 0;
-        while (number != 0) {
+   public static int  reverse(int number)
+    {
+        int  reverse = 0;
+        while(number != 0)
+        {
             int remainder = number % 10;
             reverse = reverse * 10 + remainder;
-            number = number / 10;
+            number = number/10;
         }
-        return reverse;
+       return reverse;
     }
 
 }
